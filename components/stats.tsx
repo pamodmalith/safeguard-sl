92795bbb79cf74899c89395633eef5772508773c
Pamod Malith <dev.pamodmalith@gmail.com>
2026-03-05 15:17:27 +0530

    fix: adjust max-width of StatsSection for better layout consistency

--- components/stats.tsx
+++ components/stats.tsx
@@ -1,7 +1,7 @@
 export default function StatsSection() {
   return (
     <section className="py-5">
-      <div className="mx-auto max-w-7xl space-y-8 px-6 md:space-y-16">
+      <div className="mx-auto max-w-6xl space-y-8 px-6 md:space-y-16">
         <div className="grid gap-0.5 *:text-center grid-cols-2 md:grid-cols-4 dark:[--color-muted:var(--color-zinc-900)]">
           <div className="bg-muted rounded-(--radius) space-y-4 py-12">
             <div className="text-3xl sm:text-5xl font-bold">68%</div>
