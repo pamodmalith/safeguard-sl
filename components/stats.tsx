101f5a7874304bd1460299336559714603b75a12
Pamod Malith <dev.pamodmalith@gmail.com>
2026-03-06 20:57:21 +0530

    style: minor changes

--- components/stats.tsx
+++ components/stats.tsx
@@ -1,6 +1,6 @@
 export default function StatsSection() {
   return (
-    <section className="py-5 bg-blue-600/5">
+    <section className="py-5">
       <div className="mx-auto max-w-6xl space-y-8 px-6 md:space-y-16">
         <div className="grid gap-3 *:text-center grid-cols-2 md:grid-cols-4 dark:[--color-muted:var(--color-zinc-900)]">
           <div className="bg-muted rounded-(--radius) space-y-4 py-12 border-blue-500 border shadow-md shadow-zinc-950/5">
